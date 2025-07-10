60ecee169cbc656577871f23621db318eace97e4
Njeri Kamau <faithkamau730@gmail.com>
2025-07-10 22:36:38 +0300

    pushed app-example which was being ignored by github

---
+++ prodev-mobile-app-0x00/app-example/constants/Colors.tsx
@@ -0,0 +1,9 @@
+const Colors = {
+  primary: '#1E90FF',     // Dodger Blue
+  secondary: '#FF69B4',   // Hot Pink
+  background: '#FFFFFF',  // White
+  text: '#333333',        // Dark Gray
+  muted: '#CCCCCC',       // Light Gray
+};
+
+export default Colors;
